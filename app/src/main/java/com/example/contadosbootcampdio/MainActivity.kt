c486a54830e107228c88241dbdc9d5e6e188d673
welingtongarcia <welington.unesp@gmail.com>
2021-12-16 20:04:43 -0300

    classe contato e contactsAdapter fazendo activity main tentando corrigir erros

--- app/src/main/java/com/example/contadosbootcampdio/MainActivity.kt
+++ app/src/main/java/com/example/contadosbootcampdio/MainActivity.kt
@@ -3,24 +3,28 @@ package com.example.contadosbootcampdio
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.RecyclerView
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    // necessario por causa do IF
+    val REQUEST_CONTACT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val REQUEST_CONTACT = 1// necessario por causa do IF
+
 
         // pedir permissão do usuario para acesar o contatos
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_CONTACTS),REQUEST_CONTACT)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+            !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.READ_CONTACTS),REQUEST_CONTACT)
         }else{
             setContacts()
-
-
-
         }
     }
 
@@ -29,10 +33,25 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if(requestCode==REQUEST_CONTACT)setContacts()
+        if (requestCode==REQUEST_CONTACT) setContacts()
     }
 
     private fun setContacts() {
-        TODO("Not yet implemented")
+        val contactList: ArrayList<Contact> = ArrayList()
+        val cursor  = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+        //testar se o cursor é nulo
+        if(cursor!=null){
+            while(cursor.moveToNext()){
+                contactList.add(Contact(
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)))
+            }
+        }
+        cursor.close()
     }
+
+    val adapter= ContactsAdapter(contactList)
+    val contactRecycleView = findViewById<RecyclerView>(R.id.contacts_recycle_view)
+
+    contactRecycleView.lao
 }
